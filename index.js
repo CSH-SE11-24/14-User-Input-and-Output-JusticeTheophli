2b1c2d58d9be3b5d4a9270170629c8fce1fa22bd
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-09 12:55:25 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Exercise 1: Simple Concatenation
+// Create two variables: firstName and lastName
+// Use string concatenation to display a greeting message like "Hello, John Polonia" in the console.
+
+
+
+// Exercise 2: String Interpolation
+// Create a variable called current year
+// Use string interpolation to display a message that includes the variable like "Welcome to 2023!".
+
+
+
+
+// Exercise 3: Prompt and console.log
+// Use the prompt function to ask the user for their name.
+// Display a greeting message like "Hello, [Name]!" using console log with commas
+
+
+
+
+// Exercise 4: Numeric Input and Casting
+// Use prompt to ask the user for their age as a string.
+// Convert it to a number, calculate and display their age in 10 years, like "In 10 years, you will be [age+10] years old!".
+
+
+
+
+// Exercise 5: Temperature Conversion
+// Ask the user to enter a temperature in Celsius as a string using prompt.
+// Convert it to a number, then convert and display it in Fahrenheit using the formula: Fahrenheit = (Celsius * 9/5) + 32.
+
+
+
+
